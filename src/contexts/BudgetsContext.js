750fa85b30a1bfa077496a93ee738d6b39c8d0bb
jiahoe <mrjiahoe@hotmail.com>
2022-01-15 14:18:48 +0800

    view/edit/delete expenses

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -31,6 +31,13 @@ export const BudgetsProvider = ({ children }) => {
 		});
 	}
 	function deleteBudget({ id }) {
+		setExpenses((prevExpenses) => {
+			return prevExpenses.map((expense) => {
+				if (expense.budgetId !== id) return expense;
+				return { ...expense, budgetId: UNCATEGORIZED_BUDGET_ID };
+			});
+		});
+
 		setBudgets((prevBudgets) => {
 			return prevBudgets.filter((budget) => budget.id !== id);
 		});
